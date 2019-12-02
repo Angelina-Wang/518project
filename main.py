e63e1fa9b430f011d429751c1b92b9635b2e62cf
Angelina Wang <angelina.wang@berkeley.edu>
2019-12-02 17:15:24 -0500

    skeleton

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python                                                                            
+                                                                                             
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+    def build(self, n=2):
+        switch = self.addSwitch('s1')
+        # Python's range(N) generates 0..N-1
+        for h in range(n):
+            host = self.addHost('h%s' % (h + 1))
+            self.addLink(host, switch)
+
+class BaselineTopo(Topo):
+    "Single host connected to 2 clients via 1 switch each."
+    def build(self):
+        switch1 = self.addSwitch('s1')
+        switch2 = self.addSwitch('s2')
+
+        server = self.addHost('server')
+        self.addLink(server, switch1)
+        self.addLink(server, switch2)
+
+        client1 = self.addHost('client1')
+        client2 = self.addHost('client2')
+        self.addLink(client1, switch1)
+        self.addLink(client2, switch2)
+
+def simpleTest():
+    "Create and test a simple network"
+    topo = BaselineTopo()
+    net = Mininet(topo)
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    net.pingAll()
+    net.stop()
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest()
