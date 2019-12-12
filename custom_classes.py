b5f07afde8fba302a1bc9cda9a846bf3f1774141
None <None>
2019-12-12 12:16:59 -0800

    clock works wooo

--- custom_classes.py
+++ custom_classes.py
@@ -2,14 +2,14 @@ from mininet.node import Host
 import time
 
 class HostWithTime(Host):
-	def __init__(self, start):
-		super().__init__()
-		self.start = start
+	def __init__(self, name, inNamespace=True, **params):
+		super(HostWithTime, self).__init__(name, inNamespace, **params)
+		self.start = 0
 		self.start_from_epoch = time.time()
 
 	def restart(self, start):
 		self.start = start
 		self.start_from_epoch = time.time()
 
-	def get_time(self):
+	def getTime(self):
 		return time.time() - self.start_from_epoch + self.start
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.link import TCLink
+from custom_classes import HostWithTime
 
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
@@ -21,12 +22,15 @@ class BaselineTopo(Topo):
         switch1 = self.addSwitch('s1')
         # switch2 = self.addSwitch('s2')
 
-        server = self.addHost('server')
+        #server = self.addHost('server', cls=HostWithTime)
+        server = self.addHost('server', cls=HostWithTime)
         self.addLink(server, switch1)
         #self.addLink(server, switch2)
 
-        client1 = self.addHost('client1')
-        client2 = self.addHost('client2')
+        #client1 = self.addHost('client1', cls=HostWithTime)
+        #client2 = self.addHost('client2', cls=HostWithTime)
+        client1 = self.addHost('client1', cls=HostWithTime)
+        client2 = self.addHost('client2', cls=HostWithTime)
         self.addLink(client1, switch1, bw=10, delay='5ms')
         self.addLink(client2, switch1, bw=20, delay='10ms')
 
@@ -35,14 +39,21 @@ def simpleTest():
     topo = BaselineTopo()
     net = Mininet(topo, link=TCLink)
     net.start()
+    client1, client2, server = net.get('client1', 'client2', 'server')
+    server.restart(100)
+    print 'here is the time now'
+    print server.getTime()
     print "Dumping host connections"
     dumpNodeConnections(net.hosts)
     print "Testing network connectivity"
     net.pingAll()
     print "Testing bandiwdth"
-    client1, client2, server = net.get('client1', 'client2', 'server')
-    net.iperf( (client1, server) )
-    net.iperf( (client2, server) )
+
+    print 'here is the time at the end'
+    print server.getTime()
+
+    #net.iperf( (client1, server) )
+    #net.iperf( (client2, server) )
     net.stop()
 
 if __name__ == '__main__':
