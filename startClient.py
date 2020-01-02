70daf245f46b6c0ca2d40269a0dfd594f08fec1e
None <None>
2020-01-02 15:38:33 -0800

    more

--- startClient.py
+++ startClient.py
@@ -2,5 +2,7 @@ from custom_classes import *
 
 client = AClient()
 client.connectServer('10.0.0.3')
-print(client.sendToServer('time'))
+msg1 = client.sendToServer('time')
+msg2 = client.sendToServer('hello')
+print(msg2)
 
