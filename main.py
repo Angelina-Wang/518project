5eb629e9f9337defbed7b27974b523ef20fd9a91
Angelina Wang <None>
2020-01-07 17:17:31 -0800

    more args

--- main.py
+++ main.py
@@ -29,15 +29,31 @@ class BaselineTopo(Topo):
         switch1 = self.addSwitch('s1')
         # switch2 = self.addSwitch('s2')
 
-        #server = self.addHost('server', cls=HostWithTime)
         server = self.addHost('server')
         self.addLink(server, switch1)
         #self.addLink(server, switch2)
         command = self.addHost('command')
         self.addLink(command, switch1)
 
-        #client1 = self.addHost('client1', cls=HostWithTime)
-        #client2 = self.addHost('client2', cls=HostWithTime)
+        client1 = self.addHost('client1')
+        client2 = self.addHost('client2')
+        self.addLink(client1, switch1, bw=10, delay='5ms')
+        self.addLink(client2, switch1, bw=10, delay='5ms')
+
+class BigTopo(Topo):
+    def __init__(self, num):
+        super(BigTopo, self).__init__()
+        self.numClients = num
+
+    def build(self):
+        switch1 = self.addSwitch('s1')
+        # switch2 = self.addSwitch('s2')
+
+        server = self.addHost('server')
+        self.addLink(server, switch1)
+        command = self.addHost('command')
+        self.addLink(command, switch1)
+
         client1 = self.addHost('client1')
         client2 = self.addHost('client2')
         self.addLink(client1, switch1, bw=10, delay='5ms')
@@ -53,6 +69,59 @@ def getTimes(command, client1, client2, server):
     client1Time_ = float(command.cmd('python startCommander.py {0} {1}'.format(client1.IP(), 'getTime')))
 
     return (client1Time+client1Time_)/2., (client2Time+client2Time_)/2., (serverTime+serverTime_)/2.
+
+def multiClientTest():
+    "Create and test a simple network"
+    topo = BigTopo(2)
+    net = Mininet(topo, link=TCLink)
+    net.start()
+    #asyncore.loop()
+    client1, client2, server, command = net.get('client1', 'client2', 'server', 'command')
+    print server.IP()
+    print client1.IP()
+    #client1.connectServer(server.IP())
+    #client1.connectServer('10.0.0.3')
+    #client1.sendToServer('hello')
+    # print "Dumping host connections"
+    # dumpNodeConnections(net.hosts)
+    # print "Testing network connectivity"
+    net.pingAll()
+    # print "Testing bandiwdth"
+    output1 = server.cmd('nohup python -u startServer.py > server_log.txt &')
+    print(output1)
+    output = client1.cmd('nohup python -u startClient.py {0} {1} > client1_log.txt  &'.format(server.IP(), 97))
+    print(output)
+    output = client2.cmd('nohup python -u startClient.py {0} {1} > client2_log.txt &'.format(server.IP(), 0))
+    print(output)
+    time.sleep(2)
+    
+    output2 = command.cmd('python startCommander.py {0} {1}'.format(server.IP(), 'getTime'))
+    print("get time SERVER")
+    print(output2)
+    
+    output2 = command.cmd('python startCommander.py {0} {1}'.format(client1.IP(), 'getTime'))
+    print("get time client 1")
+    print(output2)
+    
+    output2 = command.cmd('python startCommander.py {0} {1}'.format(client2.IP(), 'getTime'))
+    print("get time client 2")
+    print(output2)
+
+    output2 = command.cmd('python startCommander.py {0} {1}'.format(client1.IP(), 'startNTP'))
+    print("starting ntp 1")
+    print(output2)
+
+    output2 = command.cmd('python startCommander.py {0} {1}'.format(client2.IP(), 'startNTP'))
+    print("starting ntp 2")
+    print(output2)
+
+    a, b, c = getTimes(command, client1, client2, server)
+    print(a, b, c)
+   
+    # net.iperf((client1, server))
+    # net.iperf((client2, server))
+    net.stop()
+
 
 def simpleTest():
     "Create and test a simple network"
@@ -110,5 +179,6 @@ def simpleTest():
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
-    simpleTest()
+    multiClientTest()
+    #simpleTest()
     # testClock()
