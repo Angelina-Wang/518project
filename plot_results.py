369b2d5b8b67258cba667d1954860dc475d8839c
Angelina Wang <None>
2020-01-10 14:00:24 -0800

    all plots

--- plot_results.py
+++ plot_results.py
@@ -12,6 +12,7 @@ def multiple():
     means = []
     stds = []
     x = []
+    #for i in range(1, 11):
     for i in [2, 4, 6, 8, 10]:
         arr = pickle.load(open('multiple_{}'.format(i), 'rb'))
         num_clients = len(arr[0])
@@ -26,9 +27,43 @@ def multiple():
     plt.xlabel('Number of Clients')
     plt.ylabel('Drift from Server (s)')
     plt.title('Multiple Clients')
-    plt.xlim([1, 11])
+    plt.xlim([0, 11])
     plt.savefig('multiple.png')
-        
+
+def hierarchy():
+    for i in [2, 4, 6, 8, 10]:
+        arr = pickle.load(open('hierarchy_{}'.format(i), 'rb'))
+        num_clients = len(arr[0])
+        means = []
+        stds = []
+        x = []
+        for num in range(num_clients):
+            times = [chunk[num] for chunk in arr][:10]
+            means.append(np.mean(times))
+            stds.append(np.std(times))
+            x.append(num+1)
+        plt.errorbar(x, means, yerr=stds)
+        plt.xlabel('Depth of Hierarchy')
+        plt.ylabel('Drift from Server (s)')
+        plt.title('{}-Layer Hierarchy'.format(i))
+        plt.xlim([0, 11])
+        plt.savefig('hierarchy_{}.png'.format(i))
+        plt.close()
+
+def sendLots():
+    arr = pickle.load(open('sendLots', 'rb'))
+    means = []
+    stds = []
+    for num in range(2):
+        times = [chunk[num] for chunk in arr][:10]
+        means.append(np.mean(times))
+        stds.append(np.std(times))
+    tick_label=['Control', 'Busy Link']
+    plt.bar([0, 1], means, yerr=stds, alpha=.2)
+    plt.xticks([0, 1], tick_label)
+    plt.ylabel('Drift from Server (s)')
+    plt.title('Congested Links')
+    plt.savefig('sendLots.png')
 
 if __name__ == '__main__':
     parse = ArgumentParser()
@@ -36,5 +71,10 @@ if __name__ == '__main__':
     hps = parse.parse_args()
     if hps.version == 0:
         something()
-    if hps.version == 1:
+    elif hps.version == 1:
         multiple()
+    elif hps.version == 2:
+        hierarchy()
+    elif hps.version == 3:
+        sendLots()
+
