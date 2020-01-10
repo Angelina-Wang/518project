582b8e216bee3449ebcab57c0cb275b3e308da08
None <None>
2020-01-10 07:43:42 -0800

    added functionality for running lots of stuff on the server to degrade NTP perf

--- simulateLoad.py
+++ simulateLoad.py
@@ -1,9 +1,26 @@
+"""
+
 from multiprocessing import Pool, cpu_count
+import sys
 
 def dummy(x):
     while True:
         x*x
 
-processes = cpu_count()
+print(cpu_count())
+processes = int(cpu_count() * float(sys.argv[1]))
 pool = Pool(processes)
 pool.map(dummy, range(processes))
+"""
+
+from threading import Thread
+import sys
+
+def dummy():
+    x = 1.000000001
+    while True:
+        x*=x
+
+for i in range(sys.argv[1]):
+    t1 = Thread(target=dummy)
+    t1.setDaemon(True)
