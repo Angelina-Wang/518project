7fb03c28cdc6fe41fec31b24cc57a2f76e3dc915
Angelina Wang <None>
2020-01-10 12:24:48 -0800

    plots

--- plot_results.py
+++ plot_results.py
@@ -1,8 +1,30 @@
+import matplotlib
+
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
+import pickle
+import numpy as np
 
 def something():
     a = 1
+def multiple():
+    means = []
+    stds = []
+    x = []
+    for i in [2, 4, 6, 8, 10]:
+        arr = pickle.load(open('multiple_{}'.format(i), 'rb'))
+        num_clients = len(arr[0])
+        trials = []
+        for num in range(num_clients):
+            times = [chunk[num] for chunk in arr][:10]
+            trials.append(np.mean(times))
+        means.append(np.mean(trials))
+        stds.append(np.std(trials))
+        x.append(i)
+    plt.errorbar(x, means, yerr=stds)
+    plt.savefig('multiple.png')
+        
 
 if __name__ == '__main__':
     parse = ArgumentParser()
@@ -11,4 +33,4 @@ if __name__ == '__main__':
     if hps.version == 0:
         something()
     if hps.version == 1:
-        j
+        multiple()
